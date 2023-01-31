1236c53e1ed87bde2e1a70bbdc9e8064ef023c67
한해용 <yong7317>
2023-01-31 13:25:18 +0900

    useEffect를 더 깊게 알아보기

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,11 +1,42 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  /*
+   * 처음 렌더링될 때 한 번만 실행
+   * react가 지켜볼 것이 없으니 처음 한 번만 실행된다.
+   */
+  useEffect(() => {
+    console.log("i run only once. ex) call the api...");
+  }, []);
+  /*
+   * keyword 가 변화할 때만 실행
+   * react가 지켜볼 것이 keyword 가 있다.
+   * keyword 가 변화할 때 실행된다.
+   */
+  useEffect(() => {
+    // 첫 렌더링될 때 실행되는 것을 방지하기 위한 keyword 조건
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("i run when 'keyword' changes.", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.", counter);
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
