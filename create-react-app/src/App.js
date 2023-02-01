21f625d3fcf63933ffd15b556f45e7a536cb534e
한해용 <yong7317>
2023-02-01 11:51:11 +0900

    To Do List part One

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,41 +1,29 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  // 첫번째 방법
-  /*function destroyedFn() {
-    console.log("destroyed :(");
-  }
-  function createdFn() {
-    console.log("created :)");
-    return destroyedFn;
-  }
-  useEffect(createdFn, []);*/
-
-  // 두번째 방법
-  useEffect(() => {
-    console.log("created :)");
-    // 컴포넌트가 destory 될 때 실행 될 function
-    return () => console.log("destroyed :(");
-  }, []);
-
-  // 세번째 방법
-  /*useEffect(function () {
-    console.log("created :)");
-    return function () {
-      console.log("destroyed :(");
-    };
-  }, []);*/
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
