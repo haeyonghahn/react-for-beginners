35e7dd9bbd6f995fb9a1d4ee06faa03dfeb11709
haeyonghahn <yong80211@gmail.com>
2023-02-01 21:43:34 +0900

    7.4 Movie App part Two

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -4,10 +4,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
@@ -22,13 +23,13 @@ function App() {
         <div>
           {movies.map((movie) => (
             <div key={movie.id}>
-              <img src={movie.medium_cover_image}></img>
+              <img src={movie.medium_cover_image} alt={movie.title} />
               <h2>{movie.title}</h2>
               <p>{movie.summary}</p>
               <ul>
-                {movie.genres.map((g) => {
-                  <li key={g}>{g}</li>;
-                })}
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
               </ul>
             </div>
           ))}
